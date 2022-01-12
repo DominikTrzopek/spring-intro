7ab24ad13ec6c2cd658bc55e30dfd29b41309fd7
DominikTrzopek <79580449+DominikTrzopek@users.noreply.github.com>
2022-01-12 19:59:33 +0100

    Update RandomTextMessageService.java

--- src/main/java/com/example/RandomTextMessageService.java
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -1,11 +1,16 @@
 package com.example;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
 public class RandomTextMessageService implements MessageService {
     String[] messages = {"wiadomość 1","wiadomość 2","wiadomość 3","wiadomość 4","wiadomość 5",
             "wiadomość 6","wiadomość 7","wiadomość 8","wiadomość 9","wiadomość 10"};
 
 
-    @Override
+    @Override @Bean
     public String getMessage() {
         int rand = (int) ((Math.random() * (9)));
         return messages[rand];
