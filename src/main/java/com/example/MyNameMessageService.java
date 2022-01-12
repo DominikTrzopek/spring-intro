9bbfdd11ad0185bfbf030d5f45ca46d349071790
DominikTrzopek <79580449+DominikTrzopek@users.noreply.github.com>
2022-01-12 19:59:15 +0100

    Update MyNameMessageService.java

--- src/main/java/com/example/MyNameMessageService.java
+++ src/main/java/com/example/MyNameMessageService.java
@@ -1,9 +1,13 @@
 package com.example;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MyNameMessageService implements MessageService {
     String expectedMessage = "Dominik Trzópek";
 
-    @Override
+    @Override @Bean
     public String getMessage() {
         return expectedMessage;
     }
